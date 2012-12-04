608a23d7abd3142a0ed81836497fe6a87812d873
sebadel <seb@delneste.be>
2012-12-04 13:33:38 +0100

    iniial commit - www/ only

---
+++ www/cours/couples/couples/formation_files/formation.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100606
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('formation_files/formationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
